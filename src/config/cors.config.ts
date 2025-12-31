c07e0d6f854a9fc8770e8d57edf20b32725c212c
RaulQD <raul.g.quispe@gmail.com>
2025-12-30 20:50:18 -0500

    add cors

--- src/config/cors.config.ts
+++ src/config/cors.config.ts
@@ -2,7 +2,15 @@ import { CorsOptions } from 'cors';
 
 export const corsConfig: CorsOptions = {
   origin: (origin, callback) => {
-    const whiteList = [process.env.CORS_ORIGINS || 'http://localhost:5173'];
+    const whiteList = process.env.CORS_ORIGINS
+      ? process.env.CORS_ORIGINS.split(',').map((url) => url.trim())
+      : ['http://localhost:5173'];
+
+    if (origin && !whiteList.includes(origin)) {
+      console.info('🚫 CORS Bloqueado:', origin);
+      console.info('✅ Permitidos:', whiteList);
+    }
+
     if (!origin || whiteList.includes(origin)) {
       callback(null, true);
     } else {
